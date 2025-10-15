b6b1f334aeb0c9a6bcec48fda030aaffc92d4fa1
tomas <tomaber@stud.ntnu.no>
2025-10-15 12:10:33 +0200

    refact: added print for debug

--- preprocess.py
+++ preprocess.py
@@ -29,6 +29,9 @@ def run_preprocessing(base_data_path="dataset/braindata/*/anat/*_T2w_brain.nii.g
         subject_id = os.path.basename(os.path.dirname(os.path.dirname(img_path)))
         mask_path = os.path.join(mask_base_path, subject_id, f"{subject_id}_desc-mask_PVS.nii.gz")
 
+        print(img_path)
+        print(mask_path)
+
         if os.path.exists(mask_path):
             data.append({
                 "image": img_path,
